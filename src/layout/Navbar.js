610c18bd8e61efc667d57ba767a38d384cfaad3b
derin <derinbino@gmail.com>
2022-10-19 00:12:07 -0500

    added react router dom

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 export default function Navbar() {
@@ -6,11 +7,11 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="App.js"><img src={require('../assets/wfh-image.png')} alt="derin's logo" height="30rem"/> Derin's Full Stack App!</a>
+    <Link className="navbar-brand" to="/"><img src={require('../assets/wfh-image.png')} alt="derin's logo" height="30rem"/> Derin's Full Stack App!</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <button className='btn btn-outline-light'> Add User </button>
+    <Link className='btn btn-outline-light' to="/adduser"> Add User </Link>
   </div>
 </nav>
     </div>
